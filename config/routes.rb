95d5496e470fa6f926fd0dacec70a091beed6c42
Egor Lynko <flexoid@gmail.com>
2012-08-21 22:38:53 +0300

    Home controller and page changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Footplate::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
 
   resources :users
+
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
