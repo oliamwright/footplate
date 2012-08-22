9f6f4cfc77ea91eb82658063868b2154ca51ce09
Darrik Mazey <darrik@darmasoft.com>
2012-08-22 12:42:35 -0400

    deploy from vietnam

--- config/deploy/deploy.rb
+++ config/deploy/deploy.rb
@@ -1,5 +1,6 @@
 set :application, "footplate"
-set :repository,  "https://github.com/trueinteraction/#{application}.git"
+set :repository,  "git.darmasoft.net:/git/footplate"
+#set :repository,  "https://github.com/trueinteraction/#{application}.git"
 
 set :deploy_to, "/var/www/#{application}"
 
@@ -7,7 +8,7 @@ set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 ssh_options[:keys] = ['~/.ssh/deploy.darmasoft.com-id_rsa']
-#set :scm_username, 'git'
+set :scm_username, 'git'
 set :user, 'deploy'
 
 role :web, "nkorea.darmasoft.com"                          # Your HTTP server, Apache/etc
