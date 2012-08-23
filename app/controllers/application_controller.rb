9c45017f7896088109cd6e56c1bff291a733d21f
Egor Lynko <flexoid@gmail.com>
2012-08-23 09:39:32 +0300

    Redirect after sign up to the home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_path
   end
+
+  def after_sign_up_path_for(resource)
+    root_path
+  end
 end
