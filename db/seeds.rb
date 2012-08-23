3d96274abac053156a2e60a92197efc4e43c027e
Egor Lynko <flexoid@gmail.com>
2012-08-23 23:55:58 +0300

    Another fix in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 User.find_all_by_role('admin').destroy_all
-User.create!(email: 'admin@example.com', password: 'H9f83hf0h0', role: 'admin', bitly_username: '-', bitly_apikey: '-')
+User.create!(email: 'admin@example.com', password: 'H9f83hf0h0', role: 'admin', bitly_username: '-', bitly_apikey: '-', without_protection: true)
