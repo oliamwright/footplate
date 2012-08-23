eb01c227bfa580f2b106cb2fd87a2c5cb61f2663
Egor Lynko <flexoid@gmail.com>
2012-08-23 23:46:55 +0300

    Seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-User.create!(email: 'admin@example.com', password: 'H9f83hf0h0', bitly_username: '-', bitly_apikey: '-')
+User.find_all_by_role('admin').destroy_all
+User.create!(email: 'admin@example.com', password: 'H9f83hf0h0', role: 'admin', bitly_username: '-', bitly_apikey: '-')
